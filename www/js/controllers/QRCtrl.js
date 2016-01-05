19ae88d4ac947051667e2125e1ddc5c77a322335
Tarik MERABET <merabet.tarik@hotmail.fr>
2016-01-05 15:55:42 +0100

    0.0.3
    Changement icon, splashscreen et bug quand le résultat n'est pas une URL
    corigé.

--- www/js/controllers/QRCtrl.js
+++ www/js/controllers/QRCtrl.js
@@ -2,11 +2,24 @@ controllers.controller('QRCtrl', function(
 		$scope,
 		$cordovaBarcodeScanner,
 		$cordovaInAppBrowser,
-		$cordovaAppRate) {
+		$cordovaAppRate,
+		$ionicPopup) {
 	
 	$scope.scanBarcode = function() {
         $cordovaBarcodeScanner.scan().then(function(imageData) {
-            $cordovaInAppBrowser.open(imageData.text, "_self");
+        	if (imageData.text.substring(0, 4) == "http") {
+        	    $cordovaInAppBrowser.open(imageData.text, "_self");		
+        	} else {
+        		var myPopup = $ionicPopup.show({
+        		    title: 'Result:',
+        		    subTitle: imageData.text,
+        		    buttons: [{ text: 'OK' }, { text: 'It\'s an URL',
+        		        onTap: function(e) {
+        	        	    $cordovaInAppBrowser.open(imageData.text, "_self");
+        		        }
+        		    }]
+        		});
+        	}
         }, function(error) {
             console.error("Erreur perso: " + JSON.stringify(error));
         });
